ebed391d041dd40b06e1d974f21c26b3904e9ae7
huangjian <huangjian@bytedance.com>
2018-03-28 21:23:57 +0800

    feat(project): add mobx module
    Change-Id: Id5023c200b26c5021d59565d9c18fa42a6123c72

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { observable, autorun } from 'mobx'
+import { observable, extendObservable, autorun, computed } from 'mobx'
 import logo from './logo.svg';
 import './index.css';
 
-class Person {
-  @observable name = 'jian'
-  @observable age = 30
+class OrderLine {
+  @observable price;
+  @observable amount;
+
+  constructor() {
+    this.price = 0;
+    this.amount = 1;
+  }
+
+  @computed get total() {
+    return this.price * this.amount;
+  }
 }
 
-const person = new Person();
+const ol = new OrderLine();
 
 autorun(()=>{
-  console.log(person.name, person.age)
+  console.log(ol.price, ol.amount, ol.amount)
 })
 
-person.name = 'jian';
-person.age = 32;
+ol.price = 3
 
 
 const App = () => (
   <div className="App">
     <img className="App-Logo" src={logo} alt="React Logo" />
-    <h1 className="App-Title">Hello Parcel x React</h1>
+    <h1 className="App-Title">Hello Parcel React</h1>
   </div>
 );
 
